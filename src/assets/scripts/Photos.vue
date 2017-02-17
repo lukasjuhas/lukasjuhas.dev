3ee56307401d44baac6ce29cd13bf30dee3d64ce
Lukas Juhas <hello@lukasjuhas.com>
2017-02-17 18:50:13 +0000

    gallery

--- src/assets/scripts/Photos.vue
+++ src/assets/scripts/Photos.vue
@@ -2,11 +2,13 @@
     <div class="page">
         <div class="container">
             <h1 class="page__title">Photos</h1>
-            <div v-if="items" class="photo-feed">
-                <div v-for="item in items">
-                    <img v-bind:src="item.photo_url" v-bind:alt="item.photo_title" />
+            <div v-if="items" class="gallery">
+                <div class="gallery__item">
+                    <img :src="items[current % items.length].photo_url" v-bind:alt="items[current % items.length].photo_title" />
                 </div>
             </div>
+            <button type="button" name="button" class="gallery-arrow gallery-arrow-prev" @click="next">Prev</button>
+            <button type="button" name="button" class="gallery-arrow gallery-arrow-next" @click="next">Next</button>
         </div>
     </div>
 </template>
@@ -17,6 +19,7 @@
     data: function() {
       return {
         items: null,
+        current: 0,
       }
     },
 
@@ -38,6 +41,28 @@
           console.log(error);
         });
       },
+
+      next: function() {
+        this.current += 1;
+      },
+
+      prev: function() {
+        this.current -= 1;
+      },
     },
   };
 </script>
+
+<style lang="scss">
+  .gallery {
+    width: 100%;
+    max-height: 700px;
+    overflow: hidden;
+    text-align: center;
+    vertical-align: middle;
+
+    .gallery__item {
+
+    }
+  }
+</style>
