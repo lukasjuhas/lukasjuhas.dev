6e9ad25980a007bf8dbb0f0f456e0a9d2533e21e
Lukas Juhas <hello@lukasjuhas.com>
2017-03-08 12:26:11 +0000

    shaping photos layout for the single holiday page

--- src/assets/scripts/Holiday.vue
+++ src/assets/scripts/Holiday.vue
@@ -3,13 +3,17 @@
         <transition name="fade">
             <div v-if="item" class="container">
                 <h1 class="page__title">{{ item.title }}</h1>
-                <div class="page__content" v-html="item.content"></div>
-                <div v-if="item.photos.length" class="photos">
-                    <staggered-fade>
-                        <div v-for="(photo, index) in item.photos" class="photo" :key="index" v-bind:data-index="index">
-                            <img :src="photo.url" :alt="photo.title" />
-                        </div>
-                    </staggered-fade>
+                <div class="page__content">
+                    <div class="row">
+                        <div class="col col--xs-12 col--sm-12 col--md-7 col--lg-7" v-html="item.content"></div>
+                    </div>
+                    <div v-if="photos.length" class="photos">
+                        <staggered-fade>
+                            <div v-for="(photo, index) in photos" :style="'transform: translateX(' + (photo.parity === 'even' ? '-' : '' ) + photo.offset + 'px)'" class="photo" :class="'photo--' + photo.parity" :data-offset="photo.offset" :key="index" v-bind:data-index="index">
+                                <img :src="photo.url" :alt="photo.title" />
+                            </div>
+                        </staggered-fade>
+                    </div>
                 </div>
             </div>
         </transition>
@@ -17,9 +21,11 @@
 </template>
 
 <script>
+  import each from 'lodash/each';
   import store from './store';
   import doc from './helpers/doc';
   import StaggeredFade from './transitions/StaggeredFade.vue';
+
 
   export default {
     name: 'holiday',
@@ -40,6 +46,7 @@
       return {
         sharedState: store,
         item: null,
+        photos: [],
       }
     },
 
@@ -52,6 +59,15 @@
         this.sharedState.setLoadingAction(true);
 
         axios.get(`trips/${this.slug}`).then((response) => {
+          each(response.data.data.photos, (photo, index) => {
+            photo.offset = this.random(10, 70);
+            photo.parity = ( index & 1 ) ? 'odd' : 'even';
+
+            this.photos.push(photo);
+          });
+
+          console.log(response.data.data.photos);
+
           this.item = response.data.data;
           this.sharedState.setLoadingAction(false);
 
@@ -65,6 +81,30 @@
           }
         });
       },
+
+      random(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+      },
     },
   };
 </script>
+
+<style lang="scss">
+  .photos {
+    margin-top: $base-spacing-unit * 2;
+  }
+
+  .photo {
+    text-align: left;
+    margin-bottom: $base-spacing-unit;
+
+    img {
+      max-width: 100%;
+      height: auto;
+    }
+  }
+
+  .photo--odd {
+    text-align: right;
+  }
+</style>
