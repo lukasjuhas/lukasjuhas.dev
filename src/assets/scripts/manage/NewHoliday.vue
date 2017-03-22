3b10084aa50f00dc777734ed3abf870c92e3b37b
Lukas Juhas <hello@lukasjuhas.com>
2017-03-22 18:35:52 +0000

    wip new holiday

--- src/assets/scripts/manage/NewHoliday.vue
+++ src/assets/scripts/manage/NewHoliday.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+  import store from '../store';
+  import flash from '../helpers/flash';
   import Editor from '../components/Editor.vue';
 
   export default {
@@ -12,6 +14,37 @@
 
     components: {
       Editor,
+    },
+
+    data() {
+      return {
+        sharedState: store,
+      }
+    },
+
+    methods: {
+      saveEditor(title, content) {
+        flash.hide();
+        this.sharedState.setLoadingAction(true);
+
+        axios.post(`trips`, { title, content }).then((response) => {
+          if(response.data.error) {
+            flash.showError(response.data.error.message);
+          } else {
+            flash.showSuccess(response.data.message, true);
+          }
+
+          this.sharedState.setLoadingAction(false);
+        })
+        .catch((error, status) => {
+          if(error.status === 404) {
+            this.sharedState.state.router.replace('/404');
+          }
+
+          flash.showError('There was an unexpected problem. Please try again.');
+          this.sharedState.setLoadingAction(false);
+        });
+      },
     }
   }
 </script>
