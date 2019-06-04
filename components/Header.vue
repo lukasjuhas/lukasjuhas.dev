e384204bc8ea5172d856c9d45ed869d63a5fdaac
Lukas Juhas <itslukasjuhas@gmail.com>
2019-06-04 10:47:11 +0100

    Extract logo, add navigation into new header component

---
+++ components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+  <header class="header">
+    <nuxt-link to="/">
+      <logo :show="show"/>
+    </nuxt-link>
+
+    <navigation :show="show" />
+  </header>
+</template>
+
+<script lang="ts">
+import throttle from 'lodash/throttle';
+import { Vue, Component, Prop } from 'nuxt-property-decorator';
+import Logo from '~/components/Logo.vue';
+import Navigation from '~/components/Navigation.vue';
+
+@Component({
+  components: {
+    Logo,
+    Navigation,
+  },
+})
+export default class AppHeader extends Vue {
+  show: boolean = true;
+
+  mounted() {
+    this.handleHeader();
+  }
+
+  handleHeader(forceShow = false) {
+    if (forceShow) {
+      this.show = true;
+    }
+
+    let lastScrollTop = 0;
+    window.addEventListener(
+      'scroll',
+      throttle(() => {
+        const st = window.pageYOffset || document.documentElement.scrollTop;
+        // offset 75px
+        if (st > 75 && st > lastScrollTop) {
+          this.show = false;
+        } else {
+          this.show = true;
+        }
+        lastScrollTop = st;
+      }, 300)
+    );
+  }
+}
+</script>
---
+++ components/Navigation.vue
@@ -0,0 +1,51 @@
+<template>
+  <nav
+    :class="{'visible': show}"
+    class="nav"
+  >
+    <ul class="nav-list">
+      <li>
+        <nuxt-link to="/books">
+          Books
+        </nuxt-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator';
+
+@Component({})
+export default class Navigation extends Vue {
+  @Prop({ default: true })
+  show: boolean;
+}
+</script>
+
+<style lang="scss" scoped>
+.nav {
+  position: fixed;
+  z-index: 100;
+  top: $spacing-base;
+  right: $spacing-base;
+  transition: all $animation-speed $animation;
+  opacity: 0;
+  fill: $col-logo;
+
+  &.visible {
+    opacity: 1;
+  }
+
+  .dark & {
+    fill: $col-logo-light;
+  }
+}
+
+.nav-list {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
+
