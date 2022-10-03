78c3acfe3611156d7f69136c13c9dadb48b09605
Lukas Juhas <lukas@worksome.com>
2022-10-03 11:32:06 -0400

    add purgecss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/google-fonts",
+    "nuxt-purgecss",
+  ],
   tailwindcss: {
-    jit: true,
     // add '~tailwind.config` alias
     exposeConfig: true,
   },
@@ -32,6 +35,31 @@ export default defineNuxtConfig({
   googleFonts: {
     families: {
       Quicksand: [400],
+    },
+  },
+
+  htmlValidator: {
+    usePrettier: true,
+    failOnError: false,
+    options: {
+      extends: [
+        "html-validate:document",
+        "html-validate:recommended",
+        "html-validate:standard",
+      ],
+      rules: {
+        "svg-focusable": "off",
+        "no-unknown-elements": "error",
+        // Conflicts or not needed as we use prettier formatting
+        "void-style": "off",
+        "no-trailing-whitespace": "off",
+        // Conflict with Nuxt defaults
+        "require-sri": "off",
+        "attribute-boolean-style": "off",
+        "doctype-style": "off",
+        // Unreasonable rule
+        "no-inline-style": "off",
+      },
     },
   },
 });
