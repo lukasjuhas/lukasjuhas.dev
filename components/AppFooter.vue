874bdce8fcf07b20a95b2d5cae336e75d8bfacc9
Lukas Juhas <lukasjuhas@pm.me>
2025-01-02 12:55:36 -0500

    bump up copyright

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -2,6 +2,6 @@
   <footer
     class="absolute z-20 bottom-0 left-0 right-0 w-full h-auto p-4 text-right"
   >
-    <span class="cursor-pointer" title="&copy;2024 Lukas Juhas">&copy;</span>
+    <span class="cursor-pointer" title="&copy;2025 Lukas Juhas">&copy;</span>
   </footer>
 </template>
